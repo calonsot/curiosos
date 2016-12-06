85910293be0f190e09bcf44c948a38bc1ec54fb6
calonso-conabio <calonso@conabio.gob.mx>
2016-12-06 17:27:39 -0600

    sXVI poniendo el slide correspondiente

--- sXVI/MartinCruz.php
+++ sXVI/MartinCruz.php
@@ -137,7 +137,7 @@
 									<section>
 										<h2>Línea del tiempo</h2><br>
 										  <div class="row">
-													<div id='timeline-embed' style="width: 100%; height: 500px;" start_at_slide=""></div>
+													<div id='timeline-embed' style="width: 100%; height: 500px;" start_at_slide="7"></div>
 											</div>
 									</section>
 					</footer>
