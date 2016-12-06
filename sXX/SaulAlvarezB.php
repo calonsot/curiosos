f08c751864222d6e4e703d62faf102e1e3471692
calonso-conabio <calonso@conabio.gob.mx>
2016-12-06 16:19:38 -0600

    acomodando footer de sXX

--- sXX/SaulAlvarezB.php
+++ sXX/SaulAlvarezB.php
@@ -89,5 +89,85 @@
 				</div>
 
 			<!-- Footer -->
+				<div id="footer-wrapper">
+					<footer id="footer" class="container">
+						
+								<!-- Links -->
+                                <section>
+										<h2>Naturalistas del siglo XX</h2>
+										<div>
+											<div class="row">
+												<div class="3u 12u(mobile)">
+													<ul class="link-list last-child">
+										        <li><a href="../sXX/HeliaBravo.php">Helia Bravo Hollis</a></li>
+										        <li><a href="../sXX/EnriqueBeltran.php">Enrique Beltrán Castillo</a></li>
+										        <li><a href="../sXX/FaustinoMiranda.php">Faustino Miranda González</a></li>
+										        <li><a href="../sXX/AmeliaSamano.php">Amelia Sámano Bishop</a></li>
+										        <li><a href="../sXX/FedericoBonet.php">Federico Bonet Marco</a></li>
+										        <li><a href="../sXX/JoseAlvarez.php">José Álvarez del Villar</a></li>
+										        <li><a href="../sXX/ManuelRuiz.php">Manuel Ruiz Oronoz</a></li>
+										        <li><a href="../sXX/RafaelMCampo.php">Rafael Martín del Campo</a></li>
+										        <li><a href="../sXX/BernardoVilla.php">Bernardo Villa Ramírez</a></li>
+										        <li><a href="../sXX/EfrainHdezX.php">Efraim Hernández Xolocotzi</a></li>
+										        <li><a href="../sXX/AldoStarker.php">Aldo Starker Leopold</a></li>
+										        <li></li>
+									          </ul>
+									        </div>
+										    <div class="3u 12u(mobile)">
+										      <ul class="link-list last-child">
+										        <li><a href="../sXX/LauraHuertaM.php">Laura Huerta Múzquiz</a></li>
+										        <li><a href="../sXX/LuzMaVillareal.php">Luz María Villarreal de Puga</a></li>
+										        <li><a href="../sXX/LeonilaVazquez.php">Leonila Vázquez García</a></li>
+										        <li><a href="../sXX/MariaElenaCaso.php">María Elena Caso Muñoz</a></li>
+										        <li><a href="../sXX/DionisioPelaez.php">Dionisio Pelaez Fernández</a></li>
+										        <li><a href="../sXX/MiguelAToro.php">Miguel Álvarez del Toro</a></li>
+										        <li><a href="../sXX/AlejandroVillalobos.php">Alejandro Villalobos</a></li>
+										        <li><a href="../sXX/AnaHoffman.php">Ana Hoffman Mendizábal</a></li>
+										        <li><a href="../sXX/TeofiloHerrera.php">Teófilo Herrera</a></li>
+										        <li><a href="../sXX/AgustinAyala.php">Agustín Ayala Castañares</a></li>
+										        <li><a href="../sXX/JerzyRzedowski.php">Jerzy Rzedowski</a></li>
+										        <li></li>
+									          </ul>
+									        </div>
+										    <div class="3u 12u(mobile)">
+										      <ul class="link-list last-child">
+										        <li><a href="../sXX/AlfredoBarrera.php">Alfredo Barrera Marín</a></li>
+										        <li><a href="../sXX/IngvarEmisson.php">Ingvar Emilsson Jonatansdottir</a></li>
+										        <li><a href="../sXX/GloriaAlencaster.php">Gloria Alencáster Ybarra</a></li>
+										        <li><a href="../sXX/EnriquetaGarcia.php">Enriqueta García</a></li>
+										        <li><a href="../sXX/JuanLCifuentes.php">Juan Luis Cifuentes Lemus</a></li>
+										        <li><a href="../sXX/JulioBerdegue.php">Julio Berdegué Aznar</a></li>
+										        <li><a href="../sXX/GonzaloHalffter.php">Gonzálo Halffter</a></li>
+										        <li><a href="../sXX/GastonGuzman.php">Gastón Guzmán Huerta</a></li>
+										        <li><a href="../sXX/ArturoGomezP.php">Arturo Gómez Pompa</a></li>
+										        <li><a href="../sXX/RamonRibaN.php">Ramón Riba y Nava Esparza</a></li>
+										        <li><a href="../sXX/SalvadorContreras.php">Salvador Contreras Balderas</a></li>
+										        <li></li>
+									          </ul>
+									        </div>
+										    <div class="3u 12u(mobile)">
+										      <ul class="link-list last-child">
+										        <li><a href="../sXX/JoseSarukhan.php">José Sarukhán Kermez</a></li>
+										        <li><a href="../sXX/FranciscoTakaki.php">Francisco Takaki Takaki</a></li>
+										        <li><a href="../sXX/MiguelAMartinez.php">Miguel Ángel Martínez Alfaro</a></li>
+										        <li><a href="../sXX/DanielLluch.php">Daniel Lluch Belda</a></li>
+										        <li><a href="../sXX/JoseLuisCastro.php">José Luis Castro Aguirre</a></li>
+										        <li><a href="../sXX/MiguelAUlloa.php">Miguel Ulloa Sosa</a></li>
+										        <li><a href="../sXX/SaulAlvarezB.php">Saul Álvarez Borrego</a></li>
+										        <li><a href="../sXX/FranciscoContreras.php">Francisco Contreras Espinosa</a></li>
+										        <li><a href="../sXX/AntonioBadan.php">Antonio Badán Dangon</a></li>
+                                                   </ul>
+                                                </div>
+											</div>
+										</div>
+									</section> <br>
+									<section>
+										<h2>Línea del tiempo</h2><br>
+										  <div class="row">
+													<div id='timeline-embed' style="width: 100%; height: 500px;" start_at_slide=""></div>
+											</div>
+									</section>
+					</footer>
+				</div>
 
 		<?php include '../Layouts/footerXX.php'?>
