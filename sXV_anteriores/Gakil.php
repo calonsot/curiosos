49927897147f7cdeecbf6f41a34a2d1318ec3787
calonso-conabio <calonso@conabio.gob.mx>
2016-12-06 16:06:02 -0600

    acomodando footer de sXV_anteriores

--- sXV_anteriores/Gakil.php
+++ sXV_anteriores/Gakil.php
@@ -100,5 +100,53 @@
 						</div>
 					</div>
 				</div>
+			
+			
 			<!-- Footer -->
+				<div id="footer-wrapper">
+					<footer id="footer" class="container">
+						
+								<!-- Links -->
+                                <section>
+										<h2>Naturalistas del siglo XV y anteriores</h2>
+										<div>
+											<div class="row">
+												<div class="3u 12u(mobile)">
+													<ul class="link-list last-child">
+														<li><a href="../sXV_anteriores/Naia.php">Naia</a></li>
+                                                        <li><a href="../sXV_anteriores/Gakil.php">Gakil</a></li>
+                                                        <li></li>
+													</ul>
+												</div>
+												<div class="3u 12u(mobile)">
+													<ul class="link-list last-child">
+														<li><a href="../sXV_anteriores/Garradejaguar.php">Garra de Jaguar</a></li>
+                                                        <li><a href="../sXV_anteriores/Nezahualcoyotl.php">Acolmiztli Nezahualcóyotl</a></li>
+                                                        <li></li>
+													</ul>
+												</div>
+												<div class="3u 12u(mobile)">
+												   <ul class="link-list last-child">
+														<li><a href="../sXV_anteriores/Moctezuma.php">Moctezuma II, Xocoyotzin</a></li>
+                                                        <li></li>
+                                                   </ul>
+                                                 </div>
+                                                 <div class="3u 12u(mobile)">
+                                                   <ul class="link-list last-child">
+                                                       <li></li>
+													</ul>
+												</div>
+											</div>
+										</div>
+									</section><br>
+									<section>
+										<h2>Línea del tiempo</h2><br>
+										  <div class="row">
+													<div id='timeline-embed' style="width: 100%; height: 500px;" start_at_slide="5"></div>
+											</div>
+									</section>
+					</footer>
+				</div>
+			
+			
 <?php include '../Layouts/footerXV_Ant.php'?>
